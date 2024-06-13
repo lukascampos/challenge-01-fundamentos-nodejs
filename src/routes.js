62c0e6ab8bb693bdae2fcbe71213290f33e83f11
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-06-13 13:28:15 -0300

    route DELETE created

--- src/routes.js
+++ src/routes.js
@@ -28,5 +28,19 @@ export const routes = [
 
          return res.writeHead(201).end();
       }
-   }
+   },
+   {
+      method: 'DELETE',
+      path: buildRoutePath('/tasks/:id'),
+      handler: (req, res) => {
+
+         const rowIndex = tasks.findIndex(row => row.id === req.params.id);
+
+         if(rowIndex > -1){
+            tasks.splice(rowIndex, 1);
+         }
+
+         return res.writeHead(204).end();
+      }
+   },
 ]
